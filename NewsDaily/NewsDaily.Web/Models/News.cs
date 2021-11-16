d4d6f61bf435e5a0520a0a4c726486134c721ecf
Modou Lamin Jatta <laboyjatta@gmail.com>
2021-11-16 09:55:00 +0000

    Added      NewsDaily/.vs/NewsDaily/config/applicationhost.config Added      NewsDaily/NewsDaily.Web/Models/News.cs Modified   NewsDaily/.vs/NewsDaily/v17/.suo Modified   NewsDaily/NewsDaily.Web/NewsDaily.Web.csproj Modified   NewsDaily/NewsDaily.Web/Services/JSonFileNewsService.cs Modified   NewsDaily/NewsDaily.Web/obj/Debug/net6.0/NewsDaily.Web.csproj.AssemblyReference.cache

---
+++ NewsDaily/NewsDaily.Web/Models/News.cs
@@ -0,0 +1,10 @@
+﻿namespace NewsDaily.Web.Models
+{
+    public class News
+    {
+        public int Id { get; set; }
+        public string Reporter { get; set; }
+        public string Headline { get; set; }
+        public string Article { get; set; }
+    }
+}
--- NewsDaily/NewsDaily.Web/Services/JSonFileNewsService.cs
+++ NewsDaily/NewsDaily.Web/Services/JSonFileNewsService.cs
@@ -1,7 +1,32 @@
-﻿namespace NewsDaily.Web.Services
-{
-    public class JSonFileNewsService
-    {
+﻿using NewsDaily.Web.Models;
+using System.Text.Json;
 
+namespace NewsDaily.Web.Services
+{
+    public class JsonFileNewsService
+    {
+        public JsonFileNewsService(IWebHostEnvironment webHostEnvironment)
+        {
+            WebHostEnvironment = webHostEnvironment;
+        }
+
+        public IWebHostEnvironment WebHostEnvironment { get; }
+
+        private string JsonFileName
+        {
+            get { return Path.Combine(WebHostEnvironment.WebRootPath, "data", "news.json"); }
+        }
+
+        public IEnumerable<News> GetProducts()
+        {
+            using (var jsonFileReader = File.OpenText(JsonFileName))
+            {
+                return JsonSerializer.Deserialize<News[]>(jsonFileReader.ReadToEnd(),
+                    new JsonSerializerOptions
+                    {
+                        PropertyNameCaseInsensitive = true
+                    });
+            }
+        }
     }
 }

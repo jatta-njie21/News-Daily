8ba9937df1244ad6ead8fc03ec622d78d8fd256f
Modou Lamin Jatta <laboyjatta@gmail.com>
2022-02-18 05:51:15 +0000

    Modified   src/UI/authors.js Modified   src/UI/index.html

--- src/UI/authors.js
+++ src/UI/authors.js
@@ -2,11 +2,18 @@ const layout = { template :
 
 `
 <div class="container">
-<!-- Stack the columns on mobile by making one full-width and the other half-width -->
 <div class="row">
   <div class="col-md-1"></div>
   <div class="col-md-10">
-    Content here
+    <div class="card" style="width: 18rem;">
+    <img src="author.jpg" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">Modou Lamin Jatta</h5>
+      <p class="card-text">Bloggin daily has helped me get in touch with my inner thoughts which i in turn share with the whole world.</p>
+      <a href="" class="btn btn-primary">Browse articles</a>
+    </div>
+    
+</div>
   </div>
   <div class="col-md-1"></div>
 </div>

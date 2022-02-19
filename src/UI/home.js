aa1a6d863098a590fe8a551196dab43beef8dbfc
Modou Lamin Jatta <laboyjatta@gmail.com>
2022-02-19 02:51:24 +0000

    Modified   src/UI/home.js

--- src/UI/home.js
+++ src/UI/home.js
@@ -10,11 +10,13 @@ const home = { template :
     <img src="image.jpg" class="img-fluid" alt="...">
 
     <article class="blog-post">
-    <h2 class="blog-post-title">Sample blog post</h2>
-    <p class="blog-post-meta">January 1, 2021 by <a href="#">Mark</a></p>
+    <h2 class="blog-post-title">Another blog post</h2>
+    <p class="blog-post-meta">December 23, 2020 by <a href="#">Jacob</a></p>
 
-    <p>This blog post shows a few different types of content that’s supported and styled with Bootstrap. Basic typography, lists, tables, images, code, and more are all supported as expected.</p>
-    <hr>
+    <p>This is some additional paragraph placeholder content. It has been written to fill the available space and show how a longer snippet of text affects the surrounding content. We'll repeat it often to keep the demonstration flowing, so be on the lookout for this exact same string of text.</p>
+    <blockquote>
+      <p>Longer quote goes here, maybe with some <strong>emphasized text</strong> in the middle of it.</p>
+    </blockquote>
     <p>This is some additional paragraph placeholder content. It has been written to fill the available space and show how a longer snippet of text affects the surrounding content. We'll repeat it often to keep the demonstration flowing, so be on the lookout for this exact same string of text.</p>
     
   </article>

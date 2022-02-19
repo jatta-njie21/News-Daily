785d1b5b8e0211cae034fd6eab93eb83c59dddbe
Modou Lamin Jatta <laboyjatta@gmail.com>
2022-02-19 17:16:28 +0000

    Added      src/Backend/DataAccessLayer/AuthorsDAL.cs Added      src/DB_Backup/BlogDaily Modified   src/Backend/.vs/Backend/DesignTimeBuild/.dtbcache.v2 Modified   src/Backend/.vs/Backend/v17/.futdcache.v1 Modified   src/Backend/.vs/Backend/v17/.suo Modified   src/Backend/BlogDaily.API/appsettings.json Modified   src/Backend/DataAccessLayer/obj/Debug/net6.0/DataAccessLayer.csproj.AssemblyReference.cache

---
+++ src/Backend/DataAccessLayer/AuthorsDAL.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer
+{
+    public class AuthorsDAL
+    {
+        private readonly IConfiguration _configuration;
+        public DateTime date { get;set; }
+
+        public AuthorsDAL(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public string GetConnectionStringFromSettings(string conn)
+        {
+            return _configuration.GetConnectionString(conn);
+        }
+
+        //public int InsertNewAuthor(string tableName)
+        //{
+        //    string query = @"INSERT INTO {tableName} "
+        //}
+
+        //public JsonResult GetAuthors(string connectionString)
+        //{
+        //    string query = @"select * from Authors";
+
+        //    DataTable dataTable = new();
+        //    string sqlDataSource = _configuration
+        //}
+    }
+}

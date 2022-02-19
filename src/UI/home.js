9866ca666aed110b854bb9b11493ec060c876644
Modou Lamin Jatta <laboyjatta@gmail.com>
2022-02-19 02:27:54 +0000

    Modified   src/UI/home.js Modified   src/UI/index.html

--- src/UI/home.js
+++ src/UI/home.js
@@ -4,12 +4,28 @@ const home = { template :
 <!-- Stack the columns on mobile by making one full-width and the other half-width -->
 <div class="row">
   <div class="col-md-1"></div>
+
   <div class="col-md-10">
     
     <img src="image.jpg" class="img-fluid" alt="...">
+
+    <p>Content here</p>
+    
+    <nav aria-label="Page navigation example">
+      <ul class="pagination">
+        <li class="page-item"><a class="page-link" href="#">Previous</a></li>
+        <li class="page-item"><a class="page-link" href="#">1</a></li>
+        <li class="page-item"><a class="page-link" href="#">2</a></li>
+        <li class="page-item"><a class="page-link" href="#">3</a></li>
+        <li class="page-item"><a class="page-link" href="#">Next</a></li>
+      </ul>
+    </nav>
   </div>
+
   <div class="col-md-1"></div>
 </div>
+
+
 </div>
 
 

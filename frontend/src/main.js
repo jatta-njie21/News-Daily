2358dee9010764e370bd5beb4a2a61d0dcbcfa80
jattanjie21 <laboyjatta@gmail.com>
2022-06-19 02:13:35 +0000

    Added apollo instance in main.js

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,5 +1,22 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import { ApolloClient, createHttpLink, InMemoryCache } from '@apollo/client/core'
+
+// HTTP connection to the API
+const httpLink = createHttpLink({
+    // You should use an absolute URL here
+    uri: 'http://localhost:8080/graphql',
+  })
+
+// Cache implementation
+const cache = new InMemoryCache()
+
+// Create the apollo client
+const apolloClient = new ApolloClient({
+  link: httpLink,
+  cache,
+})
+
 
 createApp(App).use(router).mount('#app')

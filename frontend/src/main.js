2f5dc292b4e99fd2501b6cf737fa60105feb923f
jattanjie21 <laboyjatta@gmail.com>
2022-06-19 02:23:32 +0000

    Added tailwind

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import { ApolloClient, createHttpLink, InMemoryCache } from '@apollo/client/core'
+import './index.css'
 
 // HTTP connection to the API
 const httpLink = createHttpLink({

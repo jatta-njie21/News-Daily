317a743bba2e5a0ddf40e42d46728911b83d8fce
jattanjie21 <laboyjatta@gmail.com>
2022-06-19 02:05:28 +0000

    vue project init

--- src/backend/blogdaily/blog/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Author,Post
-# Register your models here.
-
-class AuthorDisplay(admin.ModelAdmin):
-    list_display = ("id","name","specialty","followers","email","telephone")
-
-admin.site.register(Author,AuthorDisplay)
-admin.site.register(Post)

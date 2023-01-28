8111d6ce0a0df1700ff81c2bbea87c3ead7bf545
jattanjie21 <laboyjatta@gmail.com>
2023-01-28 11:57:55 +0000

    Post module init

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Category,Post,PublishedPosts
 
-# Register your models here.
+
+
+admin.site.register(Category)
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display  = ['author','category','title','publish']
+    
+    
+admin.site.register(PublishedPosts)
--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,52 @@
 from django.db import models
+from common.models import *
+from authors.models import Author
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
-# Create your models here.
+
+class Category(models.Model):
+    category    = models.CharField(max_length=255)
+    description = models.TextField(blank=True,null=True)
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+        
+    def __str__(self):
+        return self.category
+    
+    
+class Post(TimeStamp):
+    author   = models.ForeignKey(Author,on_delete=models.CASCADE)
+    category = models.ForeignKey(Category,on_delete=models.CASCADE,blank=True,null=True)
+    title    = models.CharField(max_length=255)
+    post     = models.TextField()
+    publish  = models.BooleanField(default=False)
+    
+    def __str__(self):
+        return self.title
+    
+    class Meta:
+        verbose_name_plural = 'All Posts'
+
+
+class PublishedPosts(TimeStamp):
+    post = models.OneToOneField(Post,on_delete=models.CASCADE)
+    
+    
+    # @receiver(post_save, sender=Post)
+    # def check_if_post_is_published(sender,instance,created,**kwargs):
+
+    #     all_posts = Post.objects.all()
+        
+    #     for post in all_posts:
+    #         if post.publish:
+    #             PublishedPosts.objects.create(
+    #                 post = post
+    #             )
+    
+    class Meta:
+        verbose_name_plural = 'Published Posts'
+        
+    def __str__(self):
+        return str(self.post)    
